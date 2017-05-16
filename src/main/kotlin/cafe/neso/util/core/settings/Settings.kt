144008e0a958007d772b94ba44cd53f5ca501cda
Matthew Oltendorf <matthew@oltendorf.net>
2017-05-16 00:55:50 -0700

    Somehow it compiles v2

--- src/main/kotlin/cafe/neso/util/core/settings/Settings.kt
+++ src/main/kotlin/cafe/neso/util/core/settings/Settings.kt
@@ -1,7 +1,7 @@
 package cafe.neso.util.core.settings
 
 /**
- * Created by moltendorf on 2017-05-09.
+ * Created by moltendorf on 2017-05-15.
  */
 
-interface Settings
+abstract class Settings(): SettingsInterface {}
