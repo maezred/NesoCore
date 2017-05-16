cbe95e80615098350031bc06fc24aa2590c434b4
Matthew Oltendorf <matthew@oltendorf.net>
2017-05-16 00:55:50 -0700

    Somehow it compiles
    Need to fix the monster that is these gradle scripts

--- src/main/kotlin/cafe/neso/util/core/settings/SettingsInterface.kt
+++ src/main/kotlin/cafe/neso/util/core/settings/SettingsInterface.kt
@@ -4,6 +4,5 @@ package cafe.neso.util.core.settings
  * Created by moltendorf on 2017-05-09.
  */
 
-class RootSettings: Settings {
-  val enabled = true
+interface SettingsInterface {
 }
---
+++ src/main/kotlin/cafe/neso/util/core/settings/prop.kt
@@ -0,0 +1,26 @@
+package cafe.neso.util.core.settings
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ * Created by moltendorf on 2017-05-15.
+ */
+
+class prop<in R: SettingsInterface, T>(val default: T): ReadWriteProperty<R, T?> {
+  var value = default
+
+  override operator fun getValue(thisRef: R, property: KProperty<*>): T {
+    return value
+  }
+
+  override operator fun setValue(thisRef: R, property: KProperty<*>, value: T?) {
+    this.value = value ?: default
+  }
+
+  companion object {
+    init {
+      println("Hi!")
+    }
+  }
+}
